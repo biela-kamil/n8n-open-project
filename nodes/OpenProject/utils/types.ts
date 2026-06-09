bb6a3d641424ac28a46990a95c1328a19459304e
Kamil Biela <biela.kamil@gmail.com>
2026-06-09 11:56:55 +0200

    feat(openproject): add shared HAL collection types

---
+++ nodes/OpenProject/utils/types.ts
@@ -0,0 +1,17 @@
+export type OpenProjectElement = {
+	id: number;
+	identifier: string;
+	name: string;
+	active?: boolean;
+	[key: string]: unknown;
+};
+
+export type OpenProjectCollection = {
+	total: number;
+	count: number;
+	pageSize: number;
+	offset: number;
+	_embedded: {
+		elements: OpenProjectElement[];
+	};
+};
