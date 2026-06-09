0e307be243e4213cd5f60f4cbba7f6def92d128e
Kamil Biela <biela.kamil@gmail.com>
2026-06-09 11:57:17 +0200

    feat(openproject): wire Get Many execute and project listSearch

--- nodes/OpenProject/OpenProject.node.ts
+++ nodes/OpenProject/OpenProject.node.ts
@@ -1,50 +1,76 @@
-import {INodeType, INodeTypeDescription, NodeConnectionTypes} from "n8n-workflow";
-import { projectsDescription } from './resources/projects'
-import {getProjects} from "./projects/get";
+import {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionTypes,
+	NodeOperationError,
+} from "n8n-workflow";
+import { projectsDescription } from './resources/projects';
+import { getProjects } from "./projects/get";
+import { getAll } from "./resources/projects/getAll";
 
 export class OpenProject implements INodeType {
-    description: INodeTypeDescription =  {
-        displayName: 'Open Project',
-        name: 'open-project',
-        group: ['input'],
-        version: 1,
-        description: 'Open Project Node',
-        defaults: {
-            name: 'Example'
-        },
-        credentials: [
-            {
-                name: 'openProjectApi',
-                required: true,
+	description: INodeTypeDescription = {
+		displayName: 'Open Project',
+		name: 'open-project',
+		group: ['input'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Open Project Node',
+		defaults: {
+			name: 'Open Project',
+		},
+		credentials: [
+			{
+				name: 'openProjectApi',
+				required: true,
+			},
+		],
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{ name: 'Project', value: 'project' },
+					{ name: 'Task', value: 'task' },
+				],
+				default: 'project',
+			},
+			...projectsDescription,
+		],
+	};
 
-            },
-        ],
-        inputs: [NodeConnectionTypes.Main],
-        outputs: [NodeConnectionTypes.Main],
-        properties:  [
-            {
-                displayName: 'Resource',
-                name: 'resource',
-                type: 'options',
-                noDataExpression: true,
-                options: [
-                    {
-                        name: 'Project',
-                        value: 'project'
-                    },
-                    {
-                        name: 'Task',
-                        value: 'task'
-                    },
-                ],
-                default: 'project'
-            },
-            ...projectsDescription
-        ]
-    }
-    methods: {
-        projects: {
-            getProjects
-        }
-    }
+	methods = {
+		listSearch: {
+			getProjects,
+		},
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const resource = this.getNodeParameter('resource', 0) as string;
+		const operation = this.getNodeParameter('operation', 0) as string;
+
+		const returnData: INodeExecutionData[] = [];
+
+		for (let i = 0; i < items.length; i++) {
+			if (resource === 'project' && operation === 'getAll') {
+				const projects = await getAll.call(this, i);
+				returnData.push(...projects);
+			} else {
+				throw new NodeOperationError(
+					this.getNode(),
+					`Unsupported operation: ${resource} / ${operation}`,
+					{ itemIndex: i },
+				);
+			}
+		}
+
+		return [returnData];
+	}
 }
