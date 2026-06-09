37e8498e922f79a51bd5ea7ba32d8c6692b40aa3
Kamil Biela <biela.kamil@gmail.com>
2026-06-09 11:57:17 +0200

    fix(openproject): parse HAL elements in project dropdown

--- nodes/OpenProject/projects/get.ts
+++ nodes/OpenProject/projects/get.ts
@@ -1,38 +1,40 @@
-import type {ILoadOptionsFunctions, INodeListSearchItems, INodeListSearchResult} from "n8n-workflow";
-import {openProjectRequest} from "../utils/request";
-
-type ProjectItem = {
-    id: number;
-name: string;
-};
-
-type ProjectsResponse = {
-    items: ProjectItem[];
-    total_count: number;
-};
-
+import type {
+	IDataObject,
+	ILoadOptionsFunctions,
+	INodeListSearchItems,
+	INodeListSearchResult,
+} from "n8n-workflow";
+import { openProjectRequest } from "../utils/request";
+import type { OpenProjectCollection } from "../utils/types";
 
 export async function getProjects(
-    this: ILoadOptionsFunctions,
-    filter?: string,
-    paginationToken?: string,
+	this: ILoadOptionsFunctions,
+	filter?: string,
+	paginationToken?: string,
 ): Promise<INodeListSearchResult> {
-    const page = paginationToken ? +paginationToken : 1;
-    const per_page = 100;
+	const page = paginationToken ? +paginationToken : 1;
+	const pageSize = 100;
 
-    let responseData: ProjectsResponse = {
-        items: [],
-        total_count: 0,
-    };
+	const qs: IDataObject = { offset: page, pageSize };
+	if (filter) {
+		qs.filters = JSON.stringify([
+			{ name_and_identifier: { operator: '~', values: [filter] } },
+		]);
+	}
 
-    responseData = await openProjectRequest.call(this, 'GET', '/projects')
+	const response = (await openProjectRequest.call(
+		this,
+		'GET',
+		'/projects',
+		qs,
+	)) as OpenProjectCollection;
+	const elements = response._embedded?.elements ?? [];
 
-    const results: INodeListSearchItems[] = responseData.items.map((item: ProjectItem) => ({
-        name: item.title,
-        value: item.number,
-        url: item.html_url,
-    }));
+	const results: INodeListSearchItems[] = elements.map((el) => ({
+		name: el.name,
+		value: el.id,
+	}));
 
-    const nextPaginationToken = page * per_page < responseData.total_count ? page + 1 : undefined;
-    return { results, paginationToken: nextPaginationToken };
+	const nextPaginationToken = page * pageSize < response.total ? page + 1 : undefined;
+	return { results, paginationToken: nextPaginationToken };
 }
